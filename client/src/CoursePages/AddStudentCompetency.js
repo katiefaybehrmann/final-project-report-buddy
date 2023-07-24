265069cb3a667d69669932d04fee2e03ef938c13
Katie <katiefaybehrmann@gmail.com>
2023-07-24 17:10:08 -0600

    Add Student Competency functionality

--- client/src/CoursePages/AddStudentCompetency.js
+++ client/src/CoursePages/AddStudentCompetency.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { Slider, Box, Button, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark, } from "@chakra-ui/react";
+import { FormField, Input } from "../styling/styled-components";
 
-function AddStudentCompetency(){
+function AddStudentCompetency({displayedReport, setIsAdding, reports, setReports, cc}){
+    const [notes, setNotes] = useState("")
+    const [mastery, setMastery] = useState(0)
+    const [errors, setErrors] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(`/competencies`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "notes": notes,
+                "mastery": mastery,
+                "competency_category_id": cc.id,
+                "report_id": displayedReport.id
+            })
+        })
+        .then((r) => {
+        if (r.ok) {
+            r.json()
+            .then((newComp) => {
+                console.log(newComp)
+                handleAddStudentComp(newComp)});
+            }
+          else {
+            r.json().then((err) => setErrors(err.errors));
+          }})
+    }
+
+    const handleAddStudentComp = (newComp) => {
+        const updatedComps = [...displayedReport.competencies, newComp]
+        const updatedReport = {...displayedReport, competencies: updatedComps}
+        const updatedReports = reports.map(r => r.id == updatedReport.id ? updatedReport : r)
+        setReports(updatedReports)
+        setIsAdding(false)
+    }
+
     return(
         <div>
-            <p>
-                add comp
-            </p>
+            <form onSubmit={handleSubmit}>
+                <FormField>
+                    <Input
+                        type="text"
+                        id="notes"
+                        value={notes}
+                        onChange={(e) => setNotes(e.target.value)}
+                    />
+                </FormField>
+                <FormField>
+                    <Box pt={6} pb={2}>
+                        <Slider aria-label='slider-ex-6' onChange={(val) => setMastery(val)}>
+                            <SliderMark value={50}>
+                                50
+                            </SliderMark>
+                            <SliderMark value={75} >
+                                75
+                            </SliderMark>
+                            <SliderMark value={100} >
+                                100
+                            </SliderMark>
+                            <SliderMark
+                                value={mastery}
+                                textAlign='center'
+                                bg='blue.500'
+                                color='white'
+                                mt='-10'
+                                ml='-5'
+                                w='12'
+                            >
+                                {mastery}
+                            </SliderMark>
+                            <SliderTrack>
+                                <SliderFilledTrack />
+                            </SliderTrack>
+                            <SliderThumb />
+                        </Slider>
+                    </Box>
+                </FormField>
+                <FormField>
+                    {errors}
+                </FormField>
+                <FormField>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </FormField>
+            </form>
         </div>
     )
 }
--- client/src/CoursePages/StudentReportPage.js
+++ client/src/CoursePages/StudentReportPage.js
@@ -1,37 +1,50 @@
-import React, {useContext} from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../Context";
-import { Card, CardBody, Stack, StackDivider, Box, Text, Heading } from "@chakra-ui/react";
+import { Card, CardBody, Stack, StackDivider, Box, Text, Heading, Button } from "@chakra-ui/react";
 import StudentCompetency from "./StudentCompetency";
 import AddStudentCompetency from "./AddStudentCompetency";
 
-function StudentReportPage({reports, setReports}){
-    let {course_id, id} = useParams();
+function StudentReportPage({ reports, setReports }) {
+    let { course_id, id } = useParams();
     const { user } = useContext(UserContext)
     const displayedCourse = user.courses.find(c => c.id == course_id)
     const displayedReport = reports.find(r => r.id == id)
+    const [isAdding, setIsAdding] = useState(false)
 
 
     return (
         <div>
             <h3>{displayedReport.student.name}</h3>
             <Card>
-            <CardBody>
+                <CardBody>
                     <Stack divider={<StackDivider />} spacing='4'>
                         {displayedCourse.competency_categories.map((cc) => (
                             <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                {cc.name}
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {cc.description}
-                            </Text>
-                            {displayedReport.competencies.find(comp => comp.competency_category_id === cc.id) ? (
-                                <StudentCompetency displayedReport={displayedReport} reports={reports} setReports={setReports} cc={cc}/>
-                            ) : (
-                                <AddStudentCompetency/>
-                            )}
-                        </Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                    {cc.name}
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                    {cc.description}
+                                </Text>
+                                {displayedReport.competencies.find(comp => comp.competency_category_id === cc.id) ? (
+                                    <StudentCompetency displayedReport={displayedReport} reports={reports} setReports={setReports} cc={cc} />
+                                ) : (
+                                    isAdding ? (
+                                        <AddStudentCompetency displayedReport={displayedReport} reports={reports} setReports={setReports} cc={cc} setIsAdding={setIsAdding}/>
+
+                                    ) : (
+                                        <div>
+                                            <Button onClick={() => setIsAdding((isAdding) => !isAdding)}>
+                                                <span aria-label="edit">
+                                                    Add Student Progress
+                                                </span>
+                                            </Button>
+                                        </div>
+                                    )
+
+                                )}
+                            </Box>
                         ))}
                     </Stack>
                 </CardBody>
